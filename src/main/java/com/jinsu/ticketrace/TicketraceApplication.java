6411d857cf70dddee5ca71068acc77dbd1867ef9
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2025-12-11 22:08:13 +0900

    init

---
+++ src/main/java/com/jinsu/ticketrace/TicketraceApplication.java
@@ -0,0 +1,13 @@
+package com.jinsu.ticketrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinsu/ticketrace/TicketraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinsu.ticketrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
