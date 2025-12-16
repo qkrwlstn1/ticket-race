856e6b8c080ae258f1114cff2753395fa3462cba
qkrwlstn1 <75283640+qkrwlstn1@users.noreply.github.com>
2025-12-16 22:55:46 +0900

    swagger 추가

---
+++ src/main/java/com/jinsu/ticketrace/configuration/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.jinsu.ticketrace.configuration;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/jinsu/ticketrace/configuration/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.jinsu.ticketrace.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "My API",
+                version = "v1",
+                description = "My API description"
+        )
+)
+public class SwaggerConfig {
+}
